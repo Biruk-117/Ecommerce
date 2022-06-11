cfc468bbdf44d1003e167b1857030776fd207b36
Biruk-117 <onlywork117@gmail.com>
2022-06-11 19:25:37 +0200

    Create sample user in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from 'express'
-//import res from 'express/lib/response';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 
 const app = express();
 
+mongoose.connect( process.env.MONGODB_URL || 'mongodb://localhost/easyway_db', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+
+//Sends only one product
 app.get('/api/products/:_id', (req, res) => {
-    const product = data.products.find( x => x._id === req.params._id );
+    const product = data.products.find(x => x._id === req.params._id);
 
     if (product) {
         res.send(product);
@@ -16,15 +26,27 @@ app.get('/api/products/:_id', (req, res) => {
 });
 
 
+//Sends all of the products
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
 
 
+//Sends a text ( Server is ready )
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+
+
+
+app.use( '/api/users', userRouter );
+
+
+//Error chacher
+app.use( ( err, req, res, next )=>{
+    res.status(500).send( {message: err.message } );
+} )
 
 
 const port = process.env.PORT || 5000;
